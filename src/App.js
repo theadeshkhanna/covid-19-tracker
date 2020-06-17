65e21471b58a0d522bc59d3df39e639720e1b650
Adesh Khanna <adeshkhanna271@gmail.com>
2020-06-17 12:45:20 +0530

    [added] geolocation for testing purpose

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import classes from './App.css';
 
 class App extends Component {
+
+  handleClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+      }, onerror => {
+        console.log(onerror);
+      });
+    } else {
+      console.log("not supported");
+    }
+  };
+
   render() {
     return (
       <div className={classes.App}>
           <h1>Hello React</h1>
+        <button onClick={this.handleClick}>Click here</button>
       </div>
     );
   }
