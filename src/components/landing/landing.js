73161d6637768dfac7547d93d58bdbc0abc8d698
Adesh Khanna <adeshkhanna271@gmail.com>
2020-06-18 12:21:57 +0530

    [modified] landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,12 +1,14 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
+import classes from "./landing.css";
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <button><Link to="/maps">Map View</Link></button>
-                <button><Link to="/graph">Graph View</Link></button>
+            <div className={classes.Landing}>
+                <h2>Welcome to Worlds Largest Corona Database</h2>
+                <Link to="/maps" className={classes.Button}><button>Map View</button></Link>
+                <Link to="/graph" className={classes.Button}><button>Graph View</button></Link>
             </div>
         );
     }
