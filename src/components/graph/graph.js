ada508ae1d5c15a32d52bc2c5290e658cca5870b
Adesh Khanna <adeshkhanna271@gmail.com>
2020-06-22 23:41:05 +0530

    minor changes

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,24 +1,24 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import { Switch, Route } from "react-router";
 import { Link } from "react-router-dom";
-import StateWise from "../state-wise/state-wise";
+import StateWise from "../stateWise/stateWise";
 
 class Graph extends Component {
 
     render() {
         return (
-            <div>
+            <Fragment>
                 <ul>
-                    <li><Link to="graph/state-wise">State-Wise</Link></li>
-                    <li><Link to="graph/district-wise">District-wise</Link></li>
-                    <li><Link to="graph/district-daily">District-Daily</Link></li>
+                    <li><Link to="/graph/state-wise">State-Wise</Link></li>
+                    <li><Link to="/graph/district-wise">District-wise</Link></li>
+                    <li><Link to="/graph/district-daily">District-Daily</Link></li>
                 </ul>
                 <Switch>
-                    <Route path="graph/state-wise" exact component={StateWise} />
-                    <Route path="graph/district-wise" exact component={StateWise} />
-                    <Route path="graph/district-daily" exact component={StateWise} />
+                    <Route path="/graph/state-wise" exact component={StateWise} />
+                    <Route path="/graph/district-wise" exact component={StateWise} />
+                    <Route path="/graph/district-daily" exact component={StateWise} />
                 </Switch>
-            </div>
+            </Fragment>
         );
     }
 }
