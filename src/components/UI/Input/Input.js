541ef6b6432b3825d373150748bd18d180546bd0
Adesh Khanna <adeshkhanna271@gmail.com>
2020-06-24 13:40:26 +0530

    added-drop-down menu

--- src/components/UI/Input/Input.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Input = (props) => {
-    return (
-        <option value = {props.val}>{props.val}</option>
-    );
-};
-
-export default Input;
--- src/components/districtWise/districtWise.js
+++ src/components/districtWise/districtWise.js
@@ -1,30 +1,39 @@
 import React, {Component} from "react";
 import { observer, inject } from "mobx-react";
 import Chart from "../chart/chart";
-import {toJS} from "mobx";
-import Input from "../UI/Input/Input";
 
 @inject("DistrictStore")
 @observer
 class DistrictWise extends Component {
 
+    state = {
+        selectedState: null
+    }
+
     componentDidMount() {
         this.props.DistrictStore.getDistrictData();
+    }
+
+    handleDropDownChange = (event) => {
+        this.setState({
+           selectedState: event.target.value
+        });
     }
 
     render() {
         let chart_data = null;
         if (this.props.DistrictStore.data !== null) {
             chart_data = (
-                <select>
+                <select onChange={this.handleDropDownChange}>
                     {
                         Object.keys(this.props.DistrictStore.data)
                               .filter(obj => obj !== "State Unassigned")
                               .map((o,i) => {
-                                  <Input val={o} />
+                                  return <option value = {o}>{o}</option>
                               })
                     }
                 </select>
+
             );
         } else {
             chart_data = <p>Loading!!</p>
