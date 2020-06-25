c271f081caf32f3956830450947452f996b54192
Adesh Khanna <adeshkhanna271@gmail.com>
2020-06-25 21:32:06 +0530

    removed unused imports

--- src/components/districtDaily/districtDaily.js
+++ src/components/districtDaily/districtDaily.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react"
 import {inject, observer} from "mobx-react";
 import Chart from "../chart/chart";
-import {toJS} from "mobx";
 
 @inject("DistrictDailyStore")
 @observer
