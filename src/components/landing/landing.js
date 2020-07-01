19a175c49c06dc95761211a8d41252ae9639df1b
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-01 19:29:38 +0530

    [modified] landing page

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -1,13 +1,32 @@
 import React, {Component} from "react";
 import classes from "./landing.css";
+import {inject, observer} from "mobx-react";
+import Spinner from "../Spinner/Spinner";
+import {toJS} from "mobx";
 
+@inject("StateStore")
+@observer
 class Landing extends Component {
+
+    componentDidMount() {
+        this.props.StateStore.getData();
+    };
+
     render() {
-        return (
-            <div className={classes.Landing}>
-                <h2>Welcome to Worlds Largest Corona Database</h2>
-            </div>
-        );
+        let pageData = null;
+        if (this.props.StateStore.data !== null) {
+            this.props.StateStore.data
+                .filter(obj => (obj.state === "Total"))
+                // eslint-disable-next-line array-callback-return
+                .map((o, i) => {
+                    console.log(toJS(o));
+                });
+            pageData = <p>hi</p>
+
+        } else {
+            pageData = <Spinner />
+        }
+        return pageData;
     }
 }
 
