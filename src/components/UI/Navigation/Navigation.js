551c7f1443f219589e89ef06dee48f7f0ff4d740
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-07 21:53:50 +0530

    [modified] navi

--- src/components/UI/Navigation/Navigation.js
+++ src/components/UI/Navigation/Navigation.js
@@ -4,16 +4,18 @@ import {Link} from "react-router-dom";
 
 const Navigation = () => {
     return (
-        <div className={classes.Navi}>
-            <ul className={classes.LeftNavi}>
-                <li><Link to="/" className={classes.Tag}>Home</Link></li>
-                <li><Link to="/KnowTheDeveloper" className={classes.Tag}>Know The Developer</Link></li>
-            </ul>
-            <ul className={classes.RightNavi}>
-                <li><Link to="/state-wise" className={classes.Tag}>State-Wise</Link></li>
-                <li><Link to="/district-wise" className={classes.Tag}>District-wise</Link></li>
-                <li><Link to="/district-daily" className={classes.Tag}>District-Daily</Link></li>
-            </ul>
+        <div className={classes.Navigation}>
+            <nav className={classes.Nav}>
+                <ul className={classes.LeftNavi}>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/KnowTheDeveloper">Know The Developer</Link></li>
+                </ul>
+                <ul className={classes.RightNavi}>
+                    <li><Link to="/state-wise">State-Wise</Link></li>
+                    <li><Link to="/district-wise">District-Wise</Link></li>
+                    <li><Link to="/district-daily">District-Daily</Link></li>
+                </ul>
+            </nav>
         </div>
     );
 }
