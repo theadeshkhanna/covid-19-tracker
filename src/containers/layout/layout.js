9b9dd9b262c7902cd2457646580a2f252c50f85e
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-09 21:34:43 +0530

    [added] svgs

--- src/containers/layout/layout.js
+++ src/containers/layout/layout.js
@@ -6,19 +6,39 @@ import DistrictWise from "../../components/districtWise/districtWise";
 import DistrictDaily from "../../components/districtDaily/districtDaily";
 import Navigation from "../../components/UI/Navigation/Navigation";
 import KnowTheDeveloper from "../../components/KnowTheDeveloper/KnowTheDeveloper";
+import classes from "./layout.css";
 
 class Layout extends Component {
     render() {
         return (
             <Fragment>
                 <Navigation />
-                <Switch>
-                    <Route path="/KnowTheDeveloper" exact component={KnowTheDeveloper} />
-                    <Route path="/state-wise" exact component={StateWise} />
-                    <Route path="/district-wise" exact component={DistrictWise} />
-                    <Route path="/district-daily" exact component={DistrictDaily} />
-                    <Route path="/" exact component={Landing} />
-                </Switch>
+                <div>
+                    <Switch>
+                        <Route path="/KnowTheDeveloper" exact component={KnowTheDeveloper} />
+                        <Route path="/state-wise" exact component={StateWise} />
+                        <Route path="/district-wise" exact component={DistrictWise} />
+                        <Route path="/district-daily" exact component={DistrictDaily} />
+                        <Route path="/" exact component={Landing} />
+                    </Switch>
+                    <div>
+                        <img src={require("../../assets/covid.svg")} alt="covid" className={classes.Covid}/>
+                        <img src={require("../../assets/hand soap.svg")} alt="hand soap" className={classes.HandSoap}/>
+                        <img src={require("../../assets/hand wash.svg")} alt="hand wash" className={classes.HandWash}/>
+                        <img src={require("../../assets/no hand shake.svg")} alt="hand shake" className={classes.HandShake}/>
+                        <img src={require("../../assets/mask.svg")} alt="mask" className={classes.Mask}/>
+                        <img src={require("../../assets/covid.svg")} alt="covid" className={classes.Covid}/>
+                        <img src={require("../../assets/hand soap.svg")} alt="hand soap" className={classes.HandSoap}/>
+                        <img src={require("../../assets/hand wash.svg")} alt="hand wash" className={classes.HandWash}/>
+                        <img src={require("../../assets/no hand shake.svg")} alt="hand shake" className={classes.HandShake}/>
+                        <img src={require("../../assets/mask.svg")} alt="mask" className={classes.Mask}/>
+                        <img src={require("../../assets/covid.svg")} alt="covid" className={classes.Covid}/>
+                        <img src={require("../../assets/hand soap.svg")} alt="hand soap" className={classes.HandSoap}/>
+                        <img src={require("../../assets/hand wash.svg")} alt="hand wash" className={classes.HandWash}/>
+                        <img src={require("../../assets/no hand shake.svg")} alt="hand shake" className={classes.HandShake}/>
+                        <img src={require("../../assets/mask.svg")} alt="mask" className={classes.Mask}/>
+                    </div>
+                </div>
             </Fragment>
         );
     }
