a6874fce8ce68236341c31f5ce13a2c204fb4065
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-09 12:51:47 +0530

    [added] alt attributes

--- src/components/KnowTheDeveloper/KnowTheDeveloper.js
+++ src/components/KnowTheDeveloper/KnowTheDeveloper.js
@@ -4,7 +4,7 @@ import classes from './KnowTheDeveloper.css';
 const KnowTheDeveloper = () => {
     return (
         <div className={classes.View}>
-            <img src={require("../../assets/me.jpg")} className={classes.Me}/>
+            <img src={require("../../assets/me.jpg")} className={classes.Me} alt="me"/>
             <p className={classes.Intro}>I am Adesh Khanna</p>
             <p className={classes.Intro}>and i am a web and mobile developer</p>
             <br />
@@ -20,11 +20,11 @@ const KnowTheDeveloper = () => {
             <br />
             <p className={classes.Intro}>catch me on - </p>
             <ul className={classes.Social}>
-                <li className={classes.Facebook}><a href="https://www.facebook.com/profile.php?id=100005620998347"><img src={require("../../assets/logo-facebook.svg")}/></a></li>
-                <li className={classes.Instagram}><a href="https://www.instagram.com/the_adesh_khanna/"><img src={require("../../assets/logo-instagram.svg")}/></a></li>
-                <li className={classes.Linkedin}><a href="https://www.linkedin.com/in/adesh-khanna-66060216b"><img src={require("../../assets/logo-linkedin.svg")}/></a></li>
-                <li className={classes.Stackoverflow}><a href="https://stackoverflow.com/users/11675539/adesh-khanna?tab=profile"><img src={require("../../assets/logo-stackoverflow.svg")}/></a></li>
-                <li className={classes.Github}><a href="https://github.com/theadeshkhanna"><img src={require("../../assets/logo-github.svg")}/></a></li>
+                <li className={classes.Facebook}><a href="https://www.facebook.com/profile.php?id=100005620998347"><img src={require("../../assets/logo-facebook.svg")} alt="facebook"/></a></li>
+                <li className={classes.Instagram}><a href="https://www.instagram.com/the_adesh_khanna/"><img src={require("../../assets/logo-instagram.svg")} alt="instagram"/></a></li>
+                <li className={classes.Linkedin}><a href="https://www.linkedin.com/in/adesh-khanna-66060216b"><img src={require("../../assets/logo-linkedin.svg")} alt="linkedin"/></a></li>
+                <li className={classes.Stackoverflow}><a href="https://stackoverflow.com/users/11675539/adesh-khanna?tab=profile"><img src={require("../../assets/logo-stackoverflow.svg")} alt="stackoverflow"/></a></li>
+                <li className={classes.Github}><a href="https://github.com/theadeshkhanna"><img src={require("../../assets/logo-github.svg")} alt="github"/></a></li>
             </ul>
         </div>
     );
