da6f0b9fccaa10d9a5bf77b7929d08006fe51f16
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-10 17:24:21 +0530

    [added] List

---
+++ src/components/UI/List/List.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./List.css";
+
+const List = () => {
+    return (
+        <ul>
+            <li></li>
+        </ul>
+    );
+}
+
+export default List;
