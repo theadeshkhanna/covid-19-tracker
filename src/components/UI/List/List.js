eb46083bf32b98ad6c3fd92129f2ab582e0ebc92
Adesh Khanna <adeshkhanna271@gmail.com>
2020-07-10 17:42:36 +0530

    [added] list animation

--- src/components/UI/List/List.js
+++ src/components/UI/List/List.js
@@ -3,13 +3,15 @@ import classes from "./List.css";
 
 const List = () => {
     return (
-        <ul className={classes.List}>
-            <li>Stay Home</li>
-            <li>Maintain Social Distance</li>
-            <li>Wear Mask</li>
-            <li>Be Positive</li>
-            <li>Use Sanitizer</li>
-        </ul>
+        <div className={classes.ListContainer}>
+            <ul className={classes.List}>
+                <li>Stay Home</li>
+                <li>Maintain Social Distance</li>
+                <li>Wear Mask</li>
+                <li>Be Positive</li>
+                <li>Use Sanitizer</li>
+            </ul>
+        </div>
     );
 }
 
